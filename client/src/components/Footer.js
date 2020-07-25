5c2938d97c3395697d1556b2d49906cb9ee0ca71
Amruth Juntrax <amruth.rodrigues@juntrax.com>
2020-07-25 15:16:06 +0530

    design fixes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => (
  <Container>
     <Row>
   <Col xs={6}  md={3} className="text-small">
-        <div style={{paddingTop:'10px'}}>Contact </div>
+    <div style={{paddingTop:'10px'}}>Contact </div>
         <div style={{paddingTop:'10px'}}><small><a href="tel:99166 20394">99166 20394</a> </small></div>
         <div className="mt-3" style={{paddingTop:'1px', textAlign: 'left',}}><a href="https://instagram.com/myindiaoffers"><span className="fa fa-instagram mx-2 " style={{color:"#000"}}/></a><span className="fa fa-facebook mx-2 " style={{color:"#000"}}/> <span className="fa fa-youtube-play mx-2" style={{color:"#000"}}/><span className="fa fa-envelope mx-2" style={{color:"#000"}}/><span className="fa fa-phone mx-2" style={{color:"#000"}}/></div>
         <div style={{ paddingTop:'10px'}}><small>Your address here <br/> address line 2</small></div>
